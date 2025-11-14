78a74818bac090af1eeae4182a7b4dfc95e00e79
Gabryel Duarte <gabryelduartepessanha@gmail.com>
2025-11-14 15:46:34 -0300

    ajuste header

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
+
 
 class ONG(models.Model):
     usuarios = models.ManyToManyField(User, blank=True, related_name="ongs")
@@ -7,21 +8,26 @@ class ONG(models.Model):
     nome = models.CharField(max_length=200, verbose_name="Nome da ONG")
     cnpj = models.CharField(max_length=18, unique=True)
     endereco = models.CharField(max_length=255)
-    area_atuacao = models.CharField(max_length=150, verbose_name="Área de Atuação")
-    contato = models.CharField(max_length=100, help_text="E-mail ou telefone principal")
-    
+    area_atuacao = models.CharField(
+        max_length=150, verbose_name="Área de Atuação")
+    contato = models.CharField(
+        max_length=100, help_text="E-mail ou telefone principal")
+
     def __str__(self):
         return self.nome
 
+
 class Documento(models.Model):
-    ong = models.ForeignKey(ONG, on_delete=models.CASCADE, related_name="documentos")
-    
+    ong = models.ForeignKey(ONG, on_delete=models.CASCADE,
+                            related_name="documentos")
+
     tipo = models.CharField(max_length=100, verbose_name="Tipo de Documento")
     arquivo = models.FileField(upload_to='documentos_ongs/')
     data_upload = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return f"{self.tipo} - {self.ong.nome}"
+
 
 class Avaliacao(models.Model):
     STATUS_CHOICES = [
@@ -30,21 +36,30 @@ class Avaliacao(models.Model):
         ('REJEITADO', 'Rejeitado'),
     ]
 
-    ong = models.ForeignKey(ONG, on_delete=models.CASCADE, related_name="avaliacoes")
-    
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='PENDENTE')
-    comentario = models.TextField(blank=True, null=True, verbose_name="Comentário da Avaliação")
-    data = models.DateTimeField(auto_now_add=True, verbose_name="Data da Avaliação")
-    avaliador = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    ong = models.ForeignKey(ONG, on_delete=models.CASCADE,
+                            related_name="avaliacoes")
+
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='PENDENTE')
+    comentario = models.TextField(
+        blank=True, null=True, verbose_name="Comentário da Avaliação")
+    data = models.DateTimeField(
+        auto_now_add=True, verbose_name="Data da Avaliação")
+    avaliador = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"Avaliação de {self.ong.nome} - {self.status}"
 
-class Selo(models.Model):
-    ong = models.ForeignKey(ONG, on_delete=models.CASCADE, related_name="selos")
-    avaliacao_origem = models.OneToOneField(Avaliacao, on_delete=models.PROTECT, null=True)
 
-    data_emissao = models.DateField(auto_now_add=True, verbose_name="Data de Emissão")
+class Selo(models.Model):
+    ong = models.ForeignKey(
+        ONG, on_delete=models.CASCADE, related_name="selos")
+    avaliacao_origem = models.OneToOneField(
+        Avaliacao, on_delete=models.PROTECT, null=True)
+
+    data_emissao = models.DateField(
+        auto_now_add=True, verbose_name="Data de Emissão")
     data_validade = models.DateField(verbose_name="Data de Validade")
     ativo = models.BooleanField(default=True)
 
