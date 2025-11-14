a666d5c12cbe9aadef791c4aa5738ecc70444ef1
Gabryel Duarte <gabryelduartepessanha@gmail.com>
2025-11-14 17:06:15 -0300

    conexaobd com pag admin

--- cadastro/admin.py
+++ cadastro/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import ONG
+
+
+@admin.register(ONG)
+class ONGAdmin(admin.ModelAdmin):
+    list_display = ("nome", "cidade", "representante", "cnpj")
--- cadastro/views.py
+++ cadastro/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import ONG
 
 
-def index(request):
+'''def index(request):
     return render(request, "index.html")
 
 
@@ -21,4 +21,28 @@ def registrar_ong(request):
 
 
 def cadastro_sucesso(request):
-    return render(request, "sucesso.html")
+    return render(request, "sucesso.html")'''
+
+from django.shortcuts import render, redirect
+from .models import ONG
+
+
+def registrar_ong(request):
+    if request.method == "POST":
+        nome = request.POST.get("nome")
+        cidade = request.POST.get("cidade")
+        representante = request.POST.get("representante")
+        cnpj = request.POST.get("CNPJ")
+        motivacao = request.POST.get("motivacao")
+
+        ONG.objects.create(
+            nome=nome,
+            cidade=cidade,
+            representante=representante,
+            cnpj=cnpj,
+            motivacao=motivacao
+        )
+
+        return redirect("cadastro_sucesso")
+
+    return redirect("index")
